364309849bb86047df6f54c4ddc4bbe2fe1842b9
Aronex-alive <arya120609@gmail.com>
2025-10-09 22:24:56 +0530

    #DAY_7

---
+++ #DAY_7.py
@@ -0,0 +1,11 @@
+#WAP to make a calcutor using basic arithmetic operations
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+print(a,"//",b,"=",a//b)
+print(a,"**",b,"=",a**b)
+print(a,"%",b,"=",a%b)
+print("Thank you for using this calculator")
